68fdc60c51a17759ef9c2cbd0c9295c6b277bf0d
blueteckup <bestwork8888@outlook.com>
2019-08-13 14:13:03 +0200

    part 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -12,11 +13,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
     def get_queryset(self):
         """Return the last five published questions"""
-        return Question.objects.order_by('-pub_data')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -26,8 +29,8 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.chioce_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoseNotExist):
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
@@ -39,4 +42,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results'), args = (question.id,))
+        return HttpResponseRedirect(reverse('polls:results',args = (question.id,)))
