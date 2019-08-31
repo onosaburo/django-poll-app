64096de505d6e039ddd34f5548cf690a95520087
BlueTech <44312918+BlueTechUp@users.noreply.github.com>
2019-08-31 05:02:17 +0200

    Update tests.py

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -29,7 +29,7 @@ def create_question(question_text, days):
     """
      Create a question with the given `question_text` and published the
      given number of `days` offset to now (negative for questions published
-     in the past, positive for questions that have yet to be published).
+     in the past, positive for questions that have yet to be published).  
      """
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
