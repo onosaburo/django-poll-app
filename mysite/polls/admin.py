8a971927797fed3ef3274c0bdab26ef82d05e9c3
blueteckup <bestwork8888@outlook.com>
2019-09-05 03:58:48 +0200

    customize admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
